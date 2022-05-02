e2aee0e4399b1c183bd9457e327bf69dbd207f39
geo_madness <andrewsalt.e@gmail.com>
2022-05-02 17:48:37 +0500

    style(labeler): remove unused imports

--- telefone/framework/dispatch/labeler/default.py
+++ telefone/framework/dispatch/labeler/default.py
@@ -1,5 +1,5 @@
 import re
-from typing import Any, Callable, Dict, List, Set, Tuple, Type, Union
+from typing import Any, Callable, Dict, List, Type, Union
 
 import vbml
 from telefone_types.updates import BotUpdateType
