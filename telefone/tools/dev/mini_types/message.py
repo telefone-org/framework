ee0de0ca053ee385ed7dee0c1f71aa936c66d097
geo_madness <andrewsalt.e@gmail.com>
2022-06-22 20:39:09 +0500

    refactor(mini_types/message): make shortcut methods up-to-date; remove `message_min`

--- telefone/tools/dev/mini_types/message.py
+++ telefone/tools/dev/mini_types/message.py
@@ -2,8 +2,15 @@ import typing
 
 from telefone.api import ABCAPI, API
 
-from telefone_types import StatePeer
-from telefone_types.objects import Message
+from telefone_types import APIMethods, StatePeer
+from telefone_types.objects import (
+    ForceReply,
+    InlineKeyboardMarkup,
+    Message,
+    MessageEntity,
+    ReplyKeyboardMarkup,
+    ReplyKeyboardRemove,
+)
 
 
 class MessageMin(Message):
@@ -16,48 +23,64 @@ class MessageMin(Message):
 
     async def answer(
         self,
-        text: str,
-        chat_id: typing.Optional[int] = None,
+        text: typing.Optional[str] = None,
+        parse_mode: typing.Optional[str] = None,
+        entities: typing.Optional[typing.List["MessageEntity"]] = None,
         disable_web_page_preview: typing.Optional[bool] = None,
         disable_notification: typing.Optional[bool] = None,
-        reply_markup: typing.Optional[dict] = None,
-        parse_mode: typing.Optional[str] = None,
+        protect_content: typing.Optional[bool] = None,
+        reply_to_message_id: typing.Optional[int] = None,
+        allow_sending_without_reply: typing.Optional[bool] = None,
+        reply_markup: typing.Optional[
+            typing.Union[
+                "InlineKeyboardMarkup",
+                "ReplyKeyboardMarkup",
+                "ReplyKeyboardRemove",
+                "ForceReply",
+            ]
+        ] = None,
+        **kwargs
     ) -> Message:
-        params = {k: v for k, v in locals().items() if k != "self" and v is not None}
-        params["chat_id"] = chat_id or self.chat.id
-
-        return await self.ctx_api.send_message(**params)
+        params = APIMethods.get_params(locals())
+        return await self.ctx_api.send_message(chat_id=self.chat.id, **params)
 
     async def reply(
         self,
-        text: str,
+        text: typing.Optional[str] = None,
+        parse_mode: typing.Optional[str] = None,
+        entities: typing.Optional[typing.List["MessageEntity"]] = None,
         disable_web_page_preview: typing.Optional[bool] = None,
         disable_notification: typing.Optional[bool] = None,
-        reply_markup: typing.Optional[dict] = None,
-        parse_mode: typing.Optional[str] = None,
+        protect_content: typing.Optional[bool] = None,
+        allow_sending_without_reply: typing.Optional[bool] = None,
+        reply_markup: typing.Optional[
+            typing.Union[
+                "InlineKeyboardMarkup",
+                "ReplyKeyboardMarkup",
+                "ReplyKeyboardRemove",
+                "ForceReply",
+            ]
+        ] = None,
+        **kwargs
     ) -> Message:
-        params = {k: v for k, v in locals().items() if k != "self" and v is not None}
-        params["chat_id"] = self.chat.id
-        params["reply_to_message_id"] = self.message_id
-
-        return await self.ctx_api.send_message(**params)
+        params = APIMethods.get_params(locals())
+        return await self.ctx_api.send_message(
+            chat_id=self.chat.id, reply_to_message_id=self.message_id, **params
+        )
 
     async def forward(
         self,
         chat_id: typing.Union[int, str],
+        from_chat_id: typing.Optional[typing.Union[int, str]] = None,
         disable_notification: typing.Optional[bool] = None,
+        protect_content: typing.Optional[bool] = None,
+        message_id: typing.Optional[int] = None,
+        **kwargs
     ) -> Message:
-        params = {k: v for k, v in locals().items() if k != "self" and v is not None}
-        params["from_chat_id"] = self.chat.id
-        params["message_id"] = self.message_id
-
-        return await self.ctx_api.forward_message(**params)
+        params = APIMethods.get_params(locals())
+        return await self.ctx_api.forward_message(
+            from_chat_id=self.chat.id, message_id=self.message_id, **params
+        )
 
 
 MessageMin.update_forward_refs()
-
-
-def message_min(update: dict, ctx_api: "ABCAPI") -> "MessageMin":
-    message = MessageMin(**update["message"])
-    setattr(message, "unprepared_ctx_api", ctx_api)
-    return message
