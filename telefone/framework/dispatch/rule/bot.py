effc439151302a4c0949c5c8beba3db9ce4c383d
geo_madness <andrewsalt.e@gmail.com>
2022-06-27 00:50:05 +0500

    feat(rule/bot): add callback data rule

--- telefone/framework/dispatch/rule/bot.py
+++ telefone/framework/dispatch/rule/bot.py
@@ -3,22 +3,32 @@ import re
 from abc import abstractmethod
 from typing import Awaitable, Callable, Coroutine, List, Optional, Tuple, Type, Union
 
+from telefone_types.states import BaseStateGroup, get_state_repr
+from telefone_types.updates import CallbackQueryUpdate
 from vbml import Patcher, Pattern
 
 from telefone.framework.dispatch.rule.abc import ABCRule
-from telefone.tools.mini_types.message import MessageMin
-
-from telefone_types.states import BaseStateGroup, get_state_repr
-
+from telefone.tools.mini_types import MessageMin
 
 DEFAULT_PREFIXES = ["!", "/"]
-Message = MessageMin
 
 
 class ABCMessageRule(ABCRule):
     @abstractmethod
-    async def check(self, message: Message) -> bool:
+    async def check(self, message: MessageMin) -> bool:
         pass
+
+
+class CallbackDataRule(ABCMessageRule):
+    """
+    A rule that checks if callback query is equal to or is in the list of given data
+    """
+
+    def __init__(self, data: Union[str, List[str]]):
+        self.callback_data = data
+
+    async def check(self, callback_query: CallbackQueryUpdate) -> bool:
+        return callback_query.data in self.callback_data
 
 
 class CommandRule(ABCMessageRule):
@@ -38,7 +48,7 @@ class CommandRule(ABCMessageRule):
         )
         self.sep = sep
 
-    async def check(self, message: Message) -> Union[dict, bool]:
+    async def check(self, message: MessageMin) -> Union[dict, bool]:
         for prefix in self.prefixes:
             if self.args_count == 0 and message.text == prefix + self.command_text:
                 return True
@@ -60,15 +70,15 @@ class CoroutineRule(ABCMessageRule):
     def __init__(self, coroutine: Coroutine):
         self.coro = coroutine
 
-    async def check(self, message: Message) -> Union[dict, bool]:
+    async def check(self, _) -> Union[dict, bool]:
         return await self.coro
 
 
 class FuncRule(ABCMessageRule):
-    def __init__(self, func: Callable[[Message], Union[bool, Awaitable]]):
+    def __init__(self, func: Callable[[MessageMin], Union[bool, Awaitable]]):
         self.func = func
 
-    async def check(self, message: Message) -> Union[dict, bool]:
+    async def check(self, _) -> Union[dict, bool]:
         if inspect.iscoroutinefunction(self.func):
             return await self.func(update)  # type: ignore
         return self.func(update)  # type: ignore
@@ -107,7 +117,7 @@ class LevensteinRule(ABCMessageRule):
 
         return current_row[n]
 
-    async def check(self, message: Message) -> bool:
+    async def check(self, message: MessageMin) -> bool:
         for levenstein_text in self.levenstein_texts:
             if self.distance(message.text, levenstein_text) <= self.max_distance:
                 return True
@@ -138,7 +148,7 @@ class MatchRule(ABCMessageRule):
         self.patterns = pattern
         self.patcher = patcher or self.config["vbml_patcher"]
 
-    async def check(self, message: Message) -> Union[dict, bool]:
+    async def check(self, message: MessageMin) -> Union[dict, bool]:
         for pattern in self.patterns:
             result = self.patcher.check(pattern, message.text)
             if result not in (None, False):
@@ -150,7 +160,7 @@ class MessageLengthRule(ABCMessageRule):
     def __init__(self, min_length: int):
         self.min_length = min_length
 
-    async def check(self, message: Message) -> bool:
+    async def check(self, message: MessageMin) -> bool:
         return len(message.text) >= self.min_length
 
 
@@ -158,7 +168,7 @@ class PeerRule(ABCMessageRule):
     def __init__(self, from_chat: bool = True):
         self.from_chat = from_chat
 
-    async def check(self, message: Message) -> bool:
+    async def check(self, message: MessageMin) -> bool:
         if message.chat.id != message.from_.id:
             return self.from_chat
         return not self.from_chat
@@ -178,7 +188,7 @@ class RegexRule(ABCMessageRule):
 
         self.regexp = regexp
 
-    async def check(self, message: Message) -> Union[dict, bool]:
+    async def check(self, message: MessageMin) -> Union[dict, bool]:
         for regexp in self.regexp:
             match = re.match(regexp, message.text)
             if match:
@@ -187,7 +197,7 @@ class RegexRule(ABCMessageRule):
 
 
 class ReplyMessageRule(ABCMessageRule):
-    async def check(self, message: Message) -> bool:
+    async def check(self, message: MessageMin) -> bool:
         if not message.reply_to_message:
             return False
         return True
@@ -202,7 +212,7 @@ class StateRule(ABCMessageRule):
             state = [] if state is None else [state]
         self.state = [get_state_repr(s) for s in state]
 
-    async def check(self, event: Message) -> bool:
+    async def check(self, event: MessageMin) -> bool:
         if event.state_peer is None:
             return not self.state
         return event.state_peer.state in self.state
@@ -217,7 +227,7 @@ class StateGroupRule(ABCMessageRule):
             state_group = [] if state_group is None else [state_group]
         self.state_group = state_group
 
-    async def check(self, message: Message) -> bool:
+    async def check(self, message: MessageMin) -> bool:
         if message.state_peer is None:
             return not self.state_group
         return type(message.state_peer.state) in self.state_group
