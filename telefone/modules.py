6cc6e4f2f1f145158388f232a97427863a45e858
geo_madness <andrewsalt.e@gmail.com>
2022-06-27 00:28:15 +0500

    style: run `black` and `isort`

--- telefone/modules.py
+++ telefone/modules.py
@@ -2,7 +2,6 @@ import logging
 
 from choicelib import choice_in_order
 
-
 json = choice_in_order(["ujson", "hyperjson", "orjson"], do_import=True, default="json")
 logger = choice_in_order(["loguru"], do_import=True, default="logging")
 
